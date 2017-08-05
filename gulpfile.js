7e1c7feec543a6af49eccab5fa503a747def3480
wufendi <1311124282@qq.com>
2017-08-05 15:38:23 +0800

    改变index.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
+	autoprefixer = require( 'gulp-autoprefixer' ),
 	sass = require('gulp-sass');
 	gulp.task('sass',function(){
 		gulp.src('scss/*.scss')
 		.pipe(sass())
+		.pipe(autoprefixer())
 		.pipe(gulp.dest('css'));
 	});
 	gulp.watch('scss/*.scss',['sass']);
